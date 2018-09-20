715ad2028103202f62992147c83919d3f91f50b4
Cassandra Overney <coverney@olin.edu>
2018-09-20 00:15:44 -0400

    trying to find TASBEFlowAnalytics in tutorial TravisCI

--- 01_flow_cytometry/exercises.m
+++ 01_flow_cytometry/exercises.m
@@ -3,7 +3,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % example: addpath('~/Downloads/TASBEFlowAnalytics/');
-addpath('TASBEFlowAnalytics/'); % input your-path-to-analytics
+addpath('/home/travis/build/TASBE/TASBEFlowAnalytics/'); % input your-path-to-analytics
 % turn off sanitized filename warnings:
 warning('off','TASBE:SanitizeName');
 
