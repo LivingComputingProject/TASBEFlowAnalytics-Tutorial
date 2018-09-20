dfd91b932e979d2f689833f1550dbceac0bb8179
Cassandra Overney <coverney@olin.edu>
2018-09-20 17:25:29 -0400

    adding TASBEConfig test mode to tutorial TravisCI
    trying to get rid of gnuplot commands.

--- tests/test_01_flow_cytometry.m
+++ tests/test_01_flow_cytometry.m
@@ -1,5 +1,6 @@
 function test_suite = test_01_flow_cytometry
-    % TASBEConfig.checkpoint('test');
+    addpath('../TASBEFlowAnalytics/');
+    TASBEConfig.checkpoint('test');
     try % assignment of 'localfunctions' is necessary in Matlab >= 2016
         test_functions=localfunctions();
     catch % no problem; early Matlab versions can use initTestSuite fine
