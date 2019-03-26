7b5638ac6fd1973519e3ed70faa8a50dd19d8cc5
coverney <cassandra.overney@students.olin.edu>
2019-03-26 10:34:08 -0400

    Updated batch template

--- template_colormodel/make_color_model.m
+++ template_colormodel/make_color_model.m
@@ -3,8 +3,8 @@ TASBEConfig.checkpoint('init');
 stem0312 = '../example_controls/2012-03-12_';
 
 
-beadfile = [stem0312 'Beads_P3.fcs'];
-blankfile = [stem0312 'blank_P3.fcs'];
+beadfile = DataFile(0, [stem0312 'Beads_P3.fcs']);
+blankfile = DataFile(0, [stem0312 'blank_P3.fcs']);
 
 % Autodetect gating with an N-dimensional gaussian-mixture-model
 AGP = AutogateParameters();
@@ -25,17 +25,17 @@ channels = {}; colorfiles = {};
 channels{1} = Channel('FITC-A', 488, 515, 20);
 channels{1} = setPrintName(channels{1}, 'EYFP'); % Name to print on charts
 channels{1} = setLineSpec(channels{1}, 'y'); % Color for lines, when needed
-colorfiles{1} = [stem0312 'EYFP_P3.fcs']; % If there is only one channel, the color file is optional
+colorfiles{1} = DataFile(0, [stem0312 'EYFP_P3.fcs']); % If there is only one channel, the color file is optional
 
 channels{2} = Channel('PE-Tx-Red-YG-A', 561, 610, 20);
 channels{2} = setPrintName(channels{2}, 'mKate');
 channels{2} = setLineSpec(channels{2}, 'r');
-colorfiles{2} = [stem0312 'mkate_P3.fcs'];
+colorfiles{2} = DataFile(0, [stem0312 'mkate_P3.fcs']);
 
 channels{3} = Channel('Pacific Blue-A', 405, 450, 50);
 channels{3} = setPrintName(channels{3}, 'EBFP2');
 channels{3} = setLineSpec(channels{3}, 'b');
-colorfiles{3} = [stem0312 'ebfp2_P3.fcs'];
+colorfiles{3} = DataFile(0, [stem0312 'ebfp2_P3.fcs']);
 
 % FSC and SSC channels can be added to be read unprocessed to MEFL
 % channels{4} = Channel('FSC-A', 488, 488, 10);
@@ -51,8 +51,8 @@ colorpairfiles = {};
 % Entries are: channel1, channel2, constitutive channel, filename
 % This allows channel1 and channel2 to be converted into one another.
 % If you only have two colors, you can set consitutive-channel to equal channel1 or channel2
-colorpairfiles{1} = {channels{1}, channels{2}, channels{3}, [stem0312 'mkate_EBFP2_EYFP_P3.fcs']};
-colorpairfiles{2} = {channels{1}, channels{3}, channels{2}, [stem0312 'mkate_EBFP2_EYFP_P3.fcs']};
+colorpairfiles{1} = {channels{1}, channels{2}, channels{3}, DataFile(0, [stem0312 'mkate_EBFP2_EYFP_P3.fcs'])};
+colorpairfiles{2} = {channels{1}, channels{3}, channels{2}, DataFile(0, [stem0312 'mkate_EBFP2_EYFP_P3.fcs'])};
 
 % Size bead files are used for processing FSC-A units into um equivalent diameter
 % They are optional, and will not be used if size bead file is not set
